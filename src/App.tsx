c60ba98de4656ac60cabad1c86e7ad6dedfeb17e
Devi Prasad Palakollu <prasad20697@gmail.com>
2023-01-01 13:24:53 +0530

    windows scroll effect event listener

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 
-
-
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) {
+      }
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="app bg-gray-20">
       <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
