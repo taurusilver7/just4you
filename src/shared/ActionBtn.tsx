907f39f62dd1ee357b0914cbdc85859179baeecd
Devi Prasad Palakollu <prasad20697@gmail.com>
2023-01-01 15:08:09 +0530

    action button coponnet

--- src/shared/ActionBtn.tsx
+++ src/shared/ActionBtn.tsx
@@ -9,7 +9,11 @@ type Props = {
 
 const ActionBtn = ({ children, setSelectedPage }: Props) => {
   return (
-    <AnchorLink className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white">
+    <AnchorLink
+      className="rounded-md bg-secondary-500 px-8 py-2 hover:bg-primary-500 hover:text-white"
+      onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+      href={`#${SelectedPage.ContactUs}`}
+    >
       {children}
     </AnchorLink>
   );
