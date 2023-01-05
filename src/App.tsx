11a4dacb697498568d4e11e75199dc999c880f64
Devi Prasad Palakollu <prasad20697@gmail.com>
2023-01-06 00:29:39 +0530

    root files modified

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 import Home from "./scenes/home";
 import Footer from "./scenes/footer";
+import Benefits from "./scenes/benefits";
+import OurClasses from "./scenes/ourClasses";
+import ContactUs from "./scenes/contactUs";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -13,14 +16,14 @@ function App() {
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY === 0) {
-        setSelectedPage(SelectedPage.Home);
         setTopPage(true);
+        setSelectedPage(SelectedPage.Home);
       }
       if (window.scrollY !== 0) setTopPage(false);
-
-      window.addEventListener("scroll", handleScroll);
-      return () => window.removeEventListener("scroll", handleScroll);
     };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
@@ -31,6 +34,9 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
       <Footer />
     </div>
   );
